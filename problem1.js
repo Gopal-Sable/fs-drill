bb40d70c3a50e834c73792988a3543c2a47d89d9
Gopal Sable <sablegopal123@gmail.com>
2025-02-17 09:46:46 +0530

    Problem 1 solved

---
+++ problem1.js
@@ -0,0 +1,35 @@
+// Using callbacks and the fs module's asynchronous functions, do the following:
+// 1. Create a directory of random JSON files
+// 2. Delete those files simultaneously
+
+import fs from "fs";
+
+function createDirecotry(dirName) {
+  fs.mkdir(dirName, (err) => {
+    if (err) {
+      console.log("Directory already exist or error:", err.message);
+    } else {
+      console.log("Directorry created successfully");
+    }
+  });
+}
+function createFiles(fileName, data) {
+  fs.writeFile(fileName, data, (e) => {
+    if (e) {
+      console.log(e.message);
+    } else {
+      console.log("File created successfully");
+    }
+  });
+}
+function removeFile(path) {
+  fs.rm(path, (err) => {
+    if (err) {
+      console.log(err.message);
+    } else {
+      console.log("File deleted successfully");
+    }
+  });
+}
+
+export { createFiles, removeFile, createDirecotry };
