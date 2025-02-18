861cf0e41415b1b84258307bc369401ea3982aca
Gopal Sable <sablegopal123@gmail.com>
2025-02-18 11:39:36 +0530

    refactored problem 1 code

--- problem1.js
+++ problem1.js
@@ -3,33 +3,47 @@
 // 2. Delete those files simultaneously
 
 import fs from "fs";
-
-function createDirecotry(dirName) {
+let filesCount = 5;
+function createDirecotry(dirName, filesCount, callback) {
   fs.mkdir(dirName, { recursive: true }, (err) => {
     if (err) {
       console.log("Error While creating Directory:", err.message);
     } else {
       console.log("Directorry created successfully");
+      callback(dirName, filesCount);
     }
   });
 }
-function createFiles(fileName, data) {
-  fs.writeFile(fileName, data, (e) => {
-    if (e) {
-      console.log(e.message);
-    } else {
-      console.log("File created successfully");
-    }
-  });
+function createFiles(dirName, fileCount, callback) {
+  let count = 0;
+  let fileNames = [];
+  for (let i = 1; i <= fileCount; i++) {
+    let fileName = dirName + "/file" + i + ".json";
+    fileNames.push(fileName);
+    count++;
+    let data = { fileName, number: i };
+    fs.writeFile(fileName, JSON.stringify(data), (e) => {
+      if (e) {
+        console.log(e.message);
+      } else {
+        console.log(`${fileName} created successfully`);
+      }
+    });
+  }
+  if (count == fileCount) {
+    callback(fileNames);
+  }
 }
-function removeFile(path) {
-  fs.rm(path, (err) => {
-    if (err) {
-      console.log(err.message);
-    } else {
-      console.log("File deleted successfully");
-    }
+function removeFiles(path) {
+  path.map((file) => {
+    fs.rm(file, (err) => {
+      if (err) {
+        console.log(err.message);
+      } else {
+        console.log("File deleted successfully");
+      }
+    });
   });
 }
 
-export { createFiles, removeFile, createDirecotry };
+export { createFiles, removeFiles, createDirecotry };
