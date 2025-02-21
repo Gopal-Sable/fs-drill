96895e83d942156aac53b080c4aea119a84083c2
Gopal Sable <sablegopal123@gmail.com>
2025-02-21 10:31:51 +0530

    names corrected

--- UsingConstructor/test2.js
+++
@@ -1,7 +0,0 @@
-import { createDirectory, createFiles, deleteFiles } from "./problem2.js";
-
-createDirectory("jsonDir")
-  .then((res) => res)
-  .then((res) => createFiles(res, 5))
-  .then(deleteFiles)
-  .catch((err) => console.error(err.message));
--- UsingPromises/promiseTest1.js
+++
@@ -1,6 +0,0 @@
-import { createDirectory, createFiles, removeFiles } from "./promise1.js";
-
-createDirectory("dir1")
-  .then((res) => createFiles(res, 10))
-  .then(() => removeFiles("dir1"))
-  .catch((err) => console.log("Error :", err.message));
